b410d1ae41110b3d8b53c2a5c2f81753f117c191
Donat Kabashi <donatkabashi@gmail.com>
2023-08-21 15:47:11 +0200

    Initial Commit

---
+++ TicTacToeAR/TicTacToeARApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeARApp.swift
+//  TicTacToeAR
+//
+//  Created by Donat Kabashi on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
