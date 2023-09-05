16a40422bf616d32fd1aba0f3122cf365317fe5f
Donat Kabashi <donatkabashi@gmail.com>
2023-09-05 12:48:32 +0200

    implement tap board functionality

--- TicTacToeAR/UI/TTTARView.swift
+++ TicTacToeAR/UI/TTTARView.swift
@@ -30,23 +30,65 @@ class TTTARView: ARView {
     
     @objc private func viewTapped(_ recognizer: UITapGestureRecognizer) {
         let tapLocation = recognizer.location(in: self)
-        if let entity = self.entity(at: tapLocation) as? ModelEntity, entity.name == "board" {
-            ModelEntity.loadModelAsync(named: "ttt_o")
-                .sink(receiveCompletion: { completion in
-                    switch completion {
-                    case .failure(let err): print(err.localizedDescription)
-                    default: return
-                    }
-                }, receiveValue: { [weak self] entity in
-                    entity.name = "board"
-                    entity.generateCollisionShapes(recursive: true)
-                    // x: -46, 0.274, 46,
-                    // z: -44, 3, 51 (posht)
-                    entity.position = [[-46, 0.274, 46].randomElement()!, 0, [-44, 3, 51].randomElement()!]
-                    
-                    self?.boardEntity.addChild(entity)
-                })
-                .store(in: &cancellables)
+        if let entity = self.entity(at: tapLocation) as? ModelEntity {
+            
+            switch entity.name {
+            case "topLeft":
+                ModelEntity.loadModelAsync(named: "ttt_o")
+                    .sink(receiveCompletion: { completion in
+                        switch completion {
+                        case .failure(let err): print(err.localizedDescription)
+                        default: return
+                        }
+                    }, receiveValue: { [weak self] oEntity in
+                        //entity.generateCollisionShapes(recursive: true)
+                        // x: -46, 0.274, 46,
+                        // z: -44, 3, 51 (posht)
+                        //entity.position = [[-46, 0.274, 46].randomElement()!, 0, [-44, 3, 51].randomElement()!]
+                        entity.addChild(oEntity)
+                        //self?.boardEntity.addChild(entity)
+                    })
+                    .store(in: &cancellables)
+                
+                
+            case "topCenter":
+                
+                ModelEntity.loadModelAsync(named: "ttt_x")
+                    .sink(receiveCompletion: { completion in
+                        switch completion {
+                        case .failure(let err): print(err.localizedDescription)
+                        default: return
+                        }
+                    }, receiveValue: { [weak self] xEntity in
+                        //entity.generateCollisionShapes(recursive: true)
+                        // x: -46, 0.274, 46,
+                        // z: -44, 3, 51 (posht)
+                        //entity.position = [[-46, 0.274, 46].randomElement()!, 0, [-44, 3, 51].randomElement()!]
+                        entity.addChild(xEntity)
+                        //self?.boardEntity.addChild(entity)
+                    })
+                    .store(in: &cancellables)
+                
+//                ModelEntity.loadModelAsync(named: "ttt_o")
+//                    .sink(receiveCompletion: { completion in
+//                        switch completion {
+//                        case .failure(let err): print(err.localizedDescription)
+//                        default: return
+//                        }
+//                    }, receiveValue: { [weak self] entity in
+//                        entity.name = "board"
+//                        entity.generateCollisionShapes(recursive: true)
+//                        // x: -46, 0.274, 46,
+//                        // z: -44, 3, 51 (posht)
+//                        entity.position = [[-46, 0.274, 46].randomElement()!, 0, [-44, 3, 51].randomElement()!]
+//
+//                        self?.boardEntity.addChild(entity)
+//                    })
+//                    .store(in: &cancellables)
+            default: return
+            }
+            
+           
         }
     }
     
@@ -65,6 +107,24 @@ class TTTARView: ARView {
                 entity.name = "board"
                 entity.generateCollisionShapes(recursive: true)
                 anchor.addChild(entity)
+                
+                let rectangle = MeshResource.generatePlane(width: 45.66, depth: 45.66)
+                let material = SimpleMaterial(color: .red, isMetallic: false)
+                
+                let tapEntity = ModelEntity(mesh: rectangle, materials: [material])
+                tapEntity.generateCollisionShapes(recursive: true)
+                tapEntity.name = "topLeft"
+                //tapEntity.position = [[-46, 0.274, 46].randomElement()!, 0, [-44, 3, 51].randomElement()!]
+                tapEntity.position = [-46, 0, -44]
+                anchor.addChild(tapEntity)
+                
+                let material2 = SimpleMaterial(color: .green, isMetallic: false)
+                let tapEntity2 = ModelEntity(mesh: rectangle, materials: [material2])
+                tapEntity2.generateCollisionShapes(recursive: true)
+                tapEntity2.name = "topCenter"
+                tapEntity2.position = [0.274, 0, -44]
+                anchor.addChild(tapEntity2)
+                
                 self?.scene.addAnchor(anchor)
                 self?.boardEntity = entity
             })
